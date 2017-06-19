396864aee9567a804d04a29531c8f1584ca58b24
laphuongduy <laphuongduy.vector@gmail.com>
2017-06-19 13:44:48 +0700

    Init_project

---
+++ database/migrations/2017_06_20_100122_create_product_properties_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductPropertiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_properties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('product_id', false)->nullable();
+            $table->integer('property_id', false)->nullable();
+            $table->text('value')->nullable();
+            $table->string('unit', 50)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_properties');
+    }
+}

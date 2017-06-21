a2c3fbbe080638e8c0eb23f69d2730e2b702e46c
SangTranHuu <tran.huu.sang@framgia.com>
2017-06-21 14:32:06 +0700

    layoutuser

---
+++ resources/views/customers/index.blade.php
@@ -0,0 +1,13 @@
+@extends('customers.master')
+
+@section('title')
+    <title>Trang Chu</title>
+@endsection
+
+@section('content')
+    
+@endsection
+
+@section('related')
+    {{-- @include('customers.sections.related') --}}
+@endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const { mix } = require('laravel-mix');
-
+const USER_PATH = 'resources/assets/template/customer/';
+const USER_JS_PATH = USER_PATH + 'js/';
+const USER_CSS_PATH = USER_PATH + 'css/';
 /*
  |--------------------------------------------------------------------------
  | Mix Asset Management
@@ -11,5 +13,27 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/assets/js')
+   .sass('resources/assets/sass/app.scss', 'public/assets/css');
+mix.copy(USER_PATH + 'images', 'public/assets/images');
+mix.copy(USER_PATH + 'products-images','public/assets/images/products-images');
+mix.copy(USER_PATH + 'fonts', 'public/assets/fonts');
+
+mix.styles([
+	USER_CSS_PATH + 'blogmate.css',
+	USER_CSS_PATH + 'bootstrap.min.css',
+	USER_CSS_PATH + 'fancybox.css',
+	USER_CSS_PATH + 'owl.carousel.css',
+	USER_CSS_PATH + 'owl.theme.css',
+	USER_CSS_PATH + 'revslider.css',
+	USER_CSS_PATH + 'font-awesome.css',
+	USER_CSS_PATH + 'style.css',
+], 'public/assets/css/customer.min.css');
+
+mix.scripts([
+	USER_JS_PATH + 'jquery.min.js',
+	USER_JS_PATH + 'bootstrap.min.js',
+	USER_JS_PATH + 'common.js',
+	USER_JS_PATH + 'revslider.js',
+	USER_JS_PATH + 'owl.carousel.min.js',	
+], 'public/assets/js/customer.min.js');

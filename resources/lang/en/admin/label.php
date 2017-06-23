23b182679592844bcea3859ad8bf1320f69634ba
laphuongduy <laphuongduy.vector@gmail.com>
2017-06-23 09:21:01 +0700

    aaa

---
+++ resources/lang/en/admin/label.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'global' => [
+        'admin_dash' => 'Admin | Dashboard',
+        'profile' => 'Profile',
+        'contacts' => 'Contacts',
+        'mailbox' => 'Mailbox',
+        'logout' => 'Logout',
+    ],
+
+    'manage' => [
+        'product' => [
+            'header' => 'Product Management',
+            'add' => 'Create new product',
+            'edit' => 'Edit a product',
+        ],
+        'category' => [
+            'header' => 'Category Management',
+            'add' => 'Create new category',
+            'edit' => 'Edit a category',
+        ],
+        'brand' => [
+            'header' => 'Brand Management',
+            'add' => 'Create new brand',
+            'edit' => 'Edit a brand',
+        ],
+        'user' => [
+            'header' => 'User Management',
+        ],
+    ],
+
+    'header' => [
+        'welcome' => 'Welcome to',
+    ],
+];
